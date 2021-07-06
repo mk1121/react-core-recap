cade47bea49627ea5c45379ea224fd5ffef46f23
mk1121 <marufm.3666.com>
2021-07-06 12:06:46 +0600

    5000 images fetch with api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-
+import { useEffect, useState } from "react";
 function App() {
-  return <div className="App"></div>;
+  const [album, setAlbum] = useState([]);
+
+  useEffect(() =>
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => setAlbum(data))
+  );
+  const albmHeaderSty = {
+    margin: "auto",
+    width: "70%",
+  };
+  return (
+    <div className="App">
+      <div className="albmHeader" style={albmHeaderSty}>
+        {album.map((dat) => (
+          <Myalbm
+            src={dat.thumbnailUrl}
+            key={dat.id}
+            title={dat.title}
+            url={dat.url}
+          ></Myalbm>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Myalbm(props) {
+  const albmStyle = {
+    float: "left",
+    fontSize: "10px",
+    margin: "5px",
+    padding: "5px",
+    border: "2px solid white",
+    backgroundColor: "yellow",
+  };
+  return (
+    <div className="albmClass" style={albmStyle}>
+      <a href={props.url} target="_blank" rel="noreferrer">
+        <img src={props.src} alt={props.title}></img>
+      </a>
+    </div>
+  );
 }
 
 export default App;
